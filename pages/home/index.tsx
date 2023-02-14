cf35150fe7749d2c236c1472ceb323f2370ee1c2
Jen <jenniferw@Jens-MacBook-Air.local>
2023-02-14 12:19:36 -0800

    add test items

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import { Heading, Center } from '@chakra-ui/react';
+import { Heading, Center, Container, List, ListItem } from '@chakra-ui/react';
+
+const test = [...Array(5).keys()]
 
 function Page() {
   return (
@@ -9,11 +11,34 @@ function Page() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Center>
+      <Center mb={4}>
         <Heading size="lg" data-testid="welcome-header">
           Record Store
         </Heading>
       </Center>
+      <List spacing={2}>
+        {test.map((t) => (
+          <ListItem>
+            <Container
+              key={t}
+              w="full"
+              flexDirection="row"
+              display="flex"
+              border="solid"
+              borderWidth={4}
+              borderColor="grey"
+            >
+              <Container w="50%" m={5} mr={0}>
+                Add Model
+              </Container>
+              <div style={{ border: 'solid', borderRightWidth: 4, borderColor: 'grey' }} />
+              <Container w="50%" m={5} ml={0}>
+                Add Description
+              </Container>
+            </Container>
+          </ListItem>
+        ))}
+      </List>
     </>
   );
 }
